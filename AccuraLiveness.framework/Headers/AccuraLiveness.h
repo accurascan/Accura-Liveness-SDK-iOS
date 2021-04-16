74a66a72a01414f7081c2f5f962ca42211801f93
Kevin <info@technzoer.com>
2021-04-16 14:00:45 +0530

    initial commit

---
+++ AccuraLiveness.framework/Headers/AccuraLiveness.h
@@ -0,0 +1,24 @@
+//
+//  AccuraOCR.h
+//  AccuraOCR
+//
+//  Created by Technozer on 17/05/20.
+//  Copyright © 2020 Technozer. All rights reserved.
+//
+#if !TARGET_OS_WATCH
+#import <Foundation/Foundation.h>
+
+//! Project version number for AccuraOCR.
+FOUNDATION_EXPORT double AccuraLivenessVersionNumber;
+
+//! Project version string for AccuraOCR.
+FOUNDATION_EXPORT const unsigned char AccuraLivenessVersionString[];
+
+#import <AccuraLiveness/LivenessData.h>
+#import <AccuraLiveness/NSObject+Liveness.h>
+
+#endif
+
+// In this header, you should import all the public headers of your framework using statements like #import <AccuraOCR/PublicHeader.h>
+
+
